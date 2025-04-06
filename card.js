12a1943b9978b7ed10f3c1c481af157b53770810
shayInbar <shay.inbar123@gmail.com>
2025-04-06 18:16:15 +0300

    Add files via upload

---
+++ card.js
@@ -0,0 +1,7 @@
+export function Card({ children }) {
+  return <div className="bg-white shadow-md rounded-xl p-4">{children}</div>;
+}
+
+export function CardContent({ children, className }) {
+  return <div className={className}>{children}</div>;
+}
